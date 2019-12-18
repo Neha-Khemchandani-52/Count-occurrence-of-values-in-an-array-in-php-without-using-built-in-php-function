0bb2b8b35df587930e8b8f092474712a65cf882b
Neha Khemchandani <42884876+Neha-Khemchandani-52@users.noreply.github.com>
2019-12-18 14:17:32 +0530

    Created code file for the same.

---
+++ count_array_values.php
@@ -0,0 +1,21 @@
+/***
+** Description : Count occurrence of each value present in a given input array without using php built-in function
+** Added By : Neha Khemchandani
+** Date Added : 18th Dcember,2019
+***/
+
+$inpuArray = array(3,5,5,8,7,15,3,5,2,15,5,12,25,50,5,50,25,8,8,50);
+$output_array = array();
+
+foreach ($inpuArray as $value) {
+    if (isset($output_array[$value]))
+    {
+        ++$output_array[$value];
+    }
+    else
+    {
+        $output_array[$value] = 1;
+    }
+}
+
+print_r($output_array);
